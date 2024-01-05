6a8b8c42d1e0c55c7562e06501c2bd62e0022bda
Monila8 <monicalopezarroyave@gmail.com>
2024-01-05 16:10:23 -0500

    adding book

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react';
-import BookCreate from './components/BookCreate';
+import { useState } from "react"
+import BookCreate from "./components/BookCreate"
 
 function App() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([])
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
-  };
+    const updatedBooks = [...books, { id: 123, title }]
+    setBooks(updatedBooks)
+  }
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
